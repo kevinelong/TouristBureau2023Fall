e45a4a951fca3807cf45c3c6b4cc6fb02a19ff8b
kevinelong <kevinelong@gmail.com>
2023-11-02 08:57:21 -0700

    update

--- scripts/show_activities.js
+++ scripts/show_activities.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.body.innerHTML += "Hello";
-})
+    
+    function select(id, list){
+        const e = document.createElement("select");
+        e.id = id;
+        
+        const defaultOption = document.createElement("option");
+        defaultOption.innerText = "Select One";
+        e.appendChild(defaultOption); // put it in the list
+        e.selectedIndex = 0; //SELECT FIRST ITEM AS DEFAULT
+
+        for(let i = 0; i < list.length; i++){
+            const o = document.createElement("option");
+            o.innerText = list[i];
+            e.appendChild(o);
+        }
+        
+        return e;
+    }
+
+    document.body.appendChild(select("categories", categories));
+
+}); //END DOM CONTENT LOADED
